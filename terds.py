a4c96590a859b3d6520487937362b1ee61bc5286
Bismarck Martinez <bizzyb803@gmail.com>
2026-03-07 19:34:19 -0600

    cleaning comments

--- terds.py
+++ terds.py
@@ -33,29 +33,6 @@ def terdify(string):
     return terd
 
 
-# define the coloring with an int where each digit determines
-# the color of the corresponding item in the text
-# 0- white, 1- yellow, 2- green (e.g., 20210 = green, white, green, yellow, white)
-# def color_terd(text, color_code):
-#     style_correct = Style(color="black", bgcolor="green", bold=True)
-#     style_present = Style(color="black", bgcolor="yellow", bold=True)
-#     style_absent = Style(color="black", bgcolor="white", bold=True)
-
-#     result = Text()
-#     for i, char in enumerate(text):
-#         if i < len(str(color_code)):
-#             color_digit = int(str(color_code)[i])
-#             if color_digit == 2:
-#                 result.append(char, style=style_correct)
-#             elif color_digit == 1:
-#                 result.append(char, style=style_present)
-#             else:
-#                 result.append(char, style=style_absent)
-#         else:
-#             result.append(char, style=style_absent)
-#     return result
-
-
 # Simple True or False check for win
 def is_win(word, guess):
     if word == guess:
@@ -145,7 +122,7 @@ def score_guess(word, guess):
 
 def display_win():
     console.print("")
-    console.print(terd_green("GOOD JOB"))
+    console.print(terd_green("G O O D  J O B !"))
 
 
 def display_lose(word):
