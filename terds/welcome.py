a84f54a0d3c0eb51bbc7982b38781af03d7c323b
Bismarck Martinez <bizzyb803@gmail.com>
2026-03-18 05:22:06 -0500

    remove remaining references to explicit colors

--- terds/welcome.py
+++ terds/welcome.py
@@ -4,9 +4,9 @@ from .theme import console
 def welcome():
     console.print("You have 6 attempts to guess the word.")
     console.print(
-        "- [black on green] G R E E N [/black on green]: correct letter in the correct position"
+        "[black on green]  CORRECT  [/black on green] - correct letter in the correct position"
     )
     console.print(
-        "- [black on yellow] Y E L L O W [/black on yellow]: correct letter in the wrong position"
+        "[black on yellow] MISPLACED [/black on yellow] - correct letter in the wrong position"
     )
-    console.print("- [black on white] W H I T E [/black on white]: incorrect letter\n")
+    console.print("[black on white] INCORRECT [/black on white] - incorrect letter\n")
