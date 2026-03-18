59ff3ec4f9adc7cf62f3c2d4f618e1002d9f5750
Bismarck Martinez <bizzyb803@gmail.com>
2026-03-17 21:30:01 -0500

    fix: correct terdify function imports

--- terds/__init__.py
+++ terds/__init__.py
@@ -1,7 +1,7 @@
 from .theme import console
 from .title import animate_title
 from .score_guess import score_guess
-from .terdify import terdify, terd_green, terd_yellow, terd_white
+from .terdify import terdify, terd_correct, terd_present, terd_absent
 from .welcome import welcome
 from .win_lose import is_win, display_win, display_lose
 from .alphabet import draw_alphabet, clear_alphabet
