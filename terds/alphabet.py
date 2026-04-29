a80902accb4743db481517bf46596e7c3e33de58
Bismarck Martinez <bizzyb803@gmail.com>
2026-04-29 04:04:51 -0500

    WIP: alphabet display

--- terds/alphabet.py
+++ terds/alphabet.py
@@ -3,9 +3,10 @@ import string
 from rich.text import Text
 from .theme import console
 
+
 def draw_alphabet(absent_letters: set[str]):
     cols, lines = shutil.get_terminal_size()
-    if lines < 20:
+    if lines + cols <= 40:
         return
 
     first_half = Text()
@@ -23,19 +24,24 @@ def draw_alphabet(absent_letters: set[str]):
             second_half.append(f" {letter} ")
 
     # Save cursor, move to third-to-last line, clear line
-    print(f"\033[s\033[{lines-2};0H\033[K", end="")
+    print(f"\033[s\033[{lines - 2};0H\033[K", end="")
     console.print(first_half, justify="center", end="")
 
     # Move to second-to-last line, clear line
-    print(f"\033[{lines-1};0H\033[K", end="")
+    print(f"\033[{lines - 1};0H\033[K", end="")
     console.print(second_half, justify="center", end="")
 
     # Restore cursor
     print("\033[u", end="", flush=True)
 
+
 def clear_alphabet():
     cols, lines = shutil.get_terminal_size()
-    if lines < 20:
+    if lines + cols < 20:
         return
-        
-    print(f"\033[s\033[{lines-2};0H\033[K\033[{lines-1};0H\033[K\033[u", end="", flush=True)
+
+    print(
+        f"\033[s\033[{lines - 2};0H\033[K\033[{lines - 1};0H\033[K\033[u",
+        end="",
+        flush=True,
+    )
